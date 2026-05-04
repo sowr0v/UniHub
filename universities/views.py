2a8de0b8ffacc240fd4f681d788390431a5c79c6
Samdani <samdani.penough@gmail.com>
2026-05-04 08:27:05 +0600

    admission management frontend

--- universities/views.py
+++ universities/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from .models import University
 from django.shortcuts import render, get_object_or_404
-from .models import University, Event, Admission
+from .models import University, Event
 
 def home(request):
     universities = University.objects.all()
 
-    # Location and Fee
     location_query = request.GET.get('location')
     if location_query:
         universities = universities.filter(address__icontains=location_query)
@@ -15,36 +14,26 @@ def home(request):
     if max_fee:
         universities = universities.filter(cost__lte=max_fee)
 
-    # Advanced Filters
-
-    # Degree / Course Type
     course_type = request.GET.get('course_type')
     if course_type:
         universities = universities.filter(course_type__icontains=course_type)
 
-    # Subject Group
     subject_group = request.GET.get('subject_group')
     if subject_group:
         universities = universities.filter(subject_group__icontains=subject_group)
 
-    # Language
     language = request.GET.get('language')
     if language:
         universities = universities.filter(language__icontains=language)
 
-    # Institution Type
     institution_type = request.GET.get('institution_type')
     if institution_type:
         universities = universities.filter(institution_type__icontains=institution_type)
-    # Sorting by Credit
+
     credit_system = request.GET.get('credit_system')
     if credit_system:
-        # This matches the database field to the dropdown choice
         universities = universities.filter(credit_system=credit_system)
 
-
-
-    # Sorting
     sort_by = request.GET.get('sort')
     if sort_by == 'ranking':
         universities = universities.order_by('qs_ranking')
@@ -58,12 +47,8 @@ def home(request):
     }
     return render(request, 'home.html', context)
 
-
-
 def university_detail(request, uni_id):
-    # Fetch specific university
     university = get_object_or_404(University, id=uni_id)
-
     context = {
         'university': university
     }
@@ -72,8 +57,3 @@ def university_detail(request, uni_id):
 def events_list(request):
     events = Event.objects.all()
     return render(request, 'events.html', {'events': events})
-
-def admissions_list(request):
-    # Show open admissions
-    admissions = Admission.objects.filter(is_open=True)
-    return render(request, 'admissions.html', {'admissions': admissions})
