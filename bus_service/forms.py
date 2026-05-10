55fffac574497fddacaa8263f106bdf4ffccf03b
sowr0v <mijanur.rahman.sowrov@gmail.com>
2026-05-10 20:54:06 +0600

    form edit

--- bus_service/forms.py
+++ bus_service/forms.py
@@ -1,10 +1,17 @@
-from django import forms
-from .models import BusService
+from django.db import models
 
-class BusServiceForm(forms.ModelForm):
-    class Meta:
-        model = BusService
-        fields = ['university', 'has_bus_service']
-        widgets = {
-            'has_bus_service': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
-        }
+class BusService(models.Model):
+
+    BUS_CHOICES = [
+        ('yes', 'Yes'),
+        ('no', 'No'),
+        ('upcoming', 'Up Coming'),
+        ('depend', 'Depend on Students Needed'),
+    ]
+
+    university = models.CharField(max_length=200)
+
+    has_bus_service = models.CharField(
+        max_length=20,
+        choices=BUS_CHOICES
+    )
