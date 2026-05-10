bcb33710ff83a2a051d444a5ac78f506e1d3dc89
sowr0v <mijanur.rahman.sowrov@gmail.com>
2026-05-10 21:08:01 +0600

    form edit

--- bus_service/forms.py
+++ bus_service/forms.py
@@ -1,6 +1,17 @@
-from django import forms from .models
-import BusService class BusServiceForm(forms.ModelForm):
+from django import forms
+from .models import BusService
+
+
+class BusServiceForm(forms.ModelForm):
     class Meta:
         model = BusService
         fields = ['university', 'has_bus_service']
-        widgets = { 'has_bus_service': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]), }
+
+        widgets = {
+            'has_bus_service': forms.RadioSelect(
+                choices=[
+                    (True, 'Yes'),
+                    (False, 'No'),
+                ]
+            ),
+        }
